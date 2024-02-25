1f93732a737a49c0c56fac645f14a7e8f1c31e71
Muhammad Umer Ghaus <muhammadumerghaus@gmail.com>
2024-02-25 07:59:25 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ function headerShadow() {
   const navHeader = document.getElementById("header");
 
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+    navHeader.style.boxShadow = "0 1px 6px rgba(255, 255, 255, 1)";
     navHeader.style.height = "70px";
     navHeader.style.lineHeight = "70px";
 
